01bb2f0146b5c088f7ce1ad5b727091651af2e33
ajshan23 <ajmalshahan23@gmail.com>
2024-01-20 21:00:38 +0530

    check new branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Ajmal &nbsp;
+            Gokul &nbsp;
             <span className="sm:block hidden "> | &nbsp;Shahan</span>
           </p>
         </Link>
